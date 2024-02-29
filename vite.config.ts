f31ad2bc23edc184a98c4024f19f3abf83d9048a
longthb3 <longthb3@ghtk.co>
2024-02-29 22:00:25 +0700

    docs: update lib version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { resolve } from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
