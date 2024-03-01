a2bbdbcd1146a8d256a5a841c0067400a52b2020
longthb3 <longthb3@ghtk.co>
2024-03-02 00:35:53 +0700

    style: add template color to tailwind and fix animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
+  theme: {
+    extend: {
+      colors: {
+        primary: '#040f0f',
+        secondary: '#0e3013',
+        'primary-gray': '#656665',
+        'light-green': '#289944',
+        'light-gray': '#e8e8e9'
+      },
+    },
+  },
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   plugins: [],
 };
